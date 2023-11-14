76f98dba6fb1b9501c1407d3ff17fb4756373553
Benjamin Onoja <onojapeter90@gmail.com>
2023-11-14 17:09:25 +0000

    inputed the delete button for the pricelist inn admin page

--- app.js
+++ app.js
@@ -208,6 +208,17 @@ app.delete("/delete-menu/:id", async (req,res) => {
     res.status(500).send('Internal Server Error');
   }
 });
+app.delete("/delete-price/:id", async (req,res) => {
+  const Pricelist = require("./models/pricelist.js");
+  try {
+    const DeletedPrice = await Pricelist.findByIdAndDelete(req.params.id);
+    console.log(DeletedPrice);
+    res.redirect("/admin/addPricelist")
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);

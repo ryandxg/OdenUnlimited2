dcbc9729fe075f5e45cd0684dbea3d1b05023d7c
Benjamin Onoja <onojapeter90@gmail.com>
2024-01-07 18:27:16 +0000

    changed the email to send to admin for contact us

--- controllers/contactPost.js
+++ controllers/contactPost.js
@@ -68,7 +68,7 @@ async function contactPost (req,res) {
         const toAdminEmailTemplate = toAdminMailGenerator.generate(emailToAdmin);
 
         const toAdminMailOptions = {
-            from: 'Oden Unlimited <odenreservations@gmail.com>',
+            from: 'Oden Unlimited <odenunlimited@gmail.com>',
             to: userKey,
             subject: 'New Contacts',
             html: toAdminEmailTemplate,   
